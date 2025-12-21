4b3f70e852d3f7142cb2de5809cb2b527a81eaf9
nexarift-cmd <angekellymiakouang@gmail.com>
2025-12-21 10:04:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,189 @@
 const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
+const http = require("http");
+const { Server } = require("socket.io");
 
-const io = require("socket.io")(http, {
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
   cors: {
     origin: "*"
   }
 });
 
-let users = [];
+const PORT = process.env.PORT || 3000;
+
+/* =====================
+   STOCKAGE TEMPORAIRE
+===================== */
+
+// socket.id -> username
+const users = {};
+
+// username -> socket.id
+const sockets = {};
+
+// username -> [friends]
+const friends = {};
+
+// username -> [friend requests]
+const friendRequests = {};
+
+// dmRoom -> messages
+const dmMessages = {};
+
+// groupId -> { name, members, messages }
+const groups = {};
+
+/* =====================
+   UTILS
+===================== */
+
+function getDMRoom(a, b) {
+  return [a, b].sort().join("_");
+}
+
+/* =====================
+   SOCKET.IO
+===================== */
 
 io.on("connection", (socket) => {
+  console.log("🟢 Connected:", socket.id);
 
-  socket.on("join", (username) => {
-    socket.username = username;
-    users.push(username);
-    io.emit("users", users);
+  /* ---------- LOGIN ---------- */
+  socket.on("login", (username) => {
+    users[socket.id] = username;
+    sockets[username] = socket.id;
+
+    friends[username] ??= [];
+    friendRequests[username] ??= [];
+
+    io.emit("statusUpdate", { user: username, online: true });
   });
 
-  socket.on("message", (msg) => {
-    io.emit("message", msg);
+  /* ---------- SEARCH USER ---------- */
+  socket.on("searchUser", (username, cb) => {
+    cb(Boolean(sockets[username]));
   });
 
-  socket.on("disconnect", () => {
-    if (socket.username) {
-      users = users.filter(u => u !== socket.username);
-      io.emit("users", users);
+  /* ---------- FRIEND REQUEST ---------- */
+  socket.on("sendFriendRequest", (toUser) => {
+    const fromUser = users[socket.id];
+    if (!fromUser || !sockets[toUser]) return;
+
+    if (!friendRequests[toUser].includes(fromUser)) {
+      friendRequests[toUser].push(fromUser);
+      io.to(sockets[toUser]).emit("friendRequest", fromUser);
     }
   });
 
+  socket.on("acceptFriend", (fromUser) => {
+    const toUser = users[socket.id];
+    if (!toUser) return;
+
+    friends[toUser].push(fromUser);
+    friends[fromUser].push(toUser);
+
+    friendRequests[toUser] =
+      friendRequests[toUser].filter(u => u !== fromUser);
+
+    io.to(sockets[fromUser]).emit("friendAccepted", toUser);
+  });
+
+  /* ---------- PRIVATE MESSAGES ---------- */
+  socket.on("joinDM", (friend) => {
+    const me = users[socket.id];
+    if (!me) return;
+
+    const room = getDMRoom(me, friend);
+    socket.join(room);
+
+    socket.emit("dmHistory", dmMessages[room] || []);
+  });
+
+  socket.on("privateMessage", ({ to, message }) => {
+    const from = users[socket.id];
+    if (!from || !friends[from].includes(to)) return;
+
+    const room = getDMRoom(from, to);
+    dmMessages[room] ??= [];
+
+    const msg = {
+      from,
+      message,
+      time: Date.now()
+    };
+
+    dmMessages[room].push(msg);
+    io.to(room).emit("privateMessage", msg);
+  });
+
+  /* ---------- TYPING ---------- */
+  socket.on("typing", ({ to, typing }) => {
+    const from = users[socket.id];
+    if (!from || !sockets[to]) return;
+
+    io.to(sockets[to]).emit("typing", { from, typing });
+  });
+
+  /* ---------- GROUPS ---------- */
+  socket.on("createGroup", ({ name, members }, cb) => {
+    const id = Date.now().toString();
+
+    groups[id] = {
+      name,
+      members,
+      messages: []
+    };
+
+    members.forEach(user => {
+      if (sockets[user]) {
+        io.to(sockets[user]).emit("groupCreated", {
+          id,
+          name
+        });
+      }
+    });
+
+    cb(id);
+  });
+
+  socket.on("joinGroup", (groupId) => {
+    if (!groups[groupId]) return;
+    socket.join("group_" + groupId);
+    socket.emit("groupHistory", groups[groupId].messages);
+  });
+
+  socket.on("groupMessage", ({ groupId, message }) => {
+    const from = users[socket.id];
+    if (!groups[groupId]) return;
+
+    const msg = {
+      from,
+      message,
+      time: Date.now()
+    };
+
+    groups[groupId].messages.push(msg);
+    io.to("group_" + groupId).emit("groupMessage", msg);
+  });
+
+  /* ---------- DISCONNECT ---------- */
+  socket.on("disconnect", () => {
+    const username = users[socket.id];
+    if (!username) return;
+
+    delete sockets[username];
+    delete users[socket.id];
+
+    io.emit("statusUpdate", { user: username, online: false });
+  });
 });
 
-http.listen(3000, () => {
-  console.log("Socket.IO server running");
+/* =====================
+   START SERVER
+===================== */
+
+server.listen(PORT, () => {
+  console.log("🚀 Socket.IO server running on port", PORT);
 });
